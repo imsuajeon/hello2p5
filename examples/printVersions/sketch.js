ae6a060cfc345acc0ff99f26666ce83460ea9724
Byoungjo CHOI <bjc97r@inu.ac.kr>
2025-03-19 15:55:44 +0900

    typo corrected. LVersions -> Versions

--- examples/printVersions/sketch.js
+++ examples/printVersions/sketch.js
@@ -8,7 +8,7 @@ function setup() {
 }
 
 function draw() {
-  text(" LVersions", width/10, height/4);
+  text(" Versions", width/10, height/4);
   text(" p5: " +  p5.VERSION, width/10, 2*height/4);
   text("ml5: " + ml5.version, width/10, 3*height/4);
 }
